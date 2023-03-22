3390682484e24a97f70cb2c054fb3c6c6949ed39
chethan <chethan@onnetsystems.net>
2023-03-22 18:51:10 +0530

    commit1

--- src/main/java/com/rate/demo/controller/ClientController.java
+++ src/main/java/com/rate/demo/controller/ClientController.java
@@ -23,6 +23,7 @@ public class ClientController {
 	{
 		List<ClientEntity> c=clientService.getClientData();
 		//sysgfhjrukfljhl
+		//jhgfdfghjjhg
 		return ResponseEntity.ok(c);
 		}
 	@RequestMapping(value="/addClient",method = RequestMethod.POST)
